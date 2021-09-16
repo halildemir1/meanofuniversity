8ac47617cd4440a6d853c30abcd2a8b0cefb6f7f
Yavuz Yarkin Okular <yarkinokular@gmail.com>
2021-09-16 22:40:25 +0300

    Pau fonksiyon hazır

--- meanofUniversity.py
+++ meanofUniversity.py
@@ -10,6 +10,9 @@ Created on Thu Sep 16 22:34:36 2021
 # ekonomi üniversitesi not ortalama hesaplama fonksiyonu
 
 # pamukkale üniversitesi not ortalama hesaplama fonksiyonu
+def ortalamaPau(pauVize,pauFinal):
+    pauNot=(pauVize*0,4)+(pauFinal*0,6)
+    return pauNot
 
 # while döngüsü
 # while içerisinde okul seçimi için koşul döngüsü
