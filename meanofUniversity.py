b1dc55c8bbd7596aea183ac662f38d3b7b90a70e
Yavuz Yarkin Okular <yarkinokular@gmail.com>
2021-09-16 22:37:06 +0300

    genel taslak hazır

---
+++ meanofUniversity.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 22:34:36 2021
+
+@author: yarki
+"""
+
+# program tanıtımı yapılacak yorum satırı içerisinde
+
+# ekonomi üniversitesi not ortalama hesaplama fonksiyonu
+
+# pamukkale üniversitesi not ortalama hesaplama fonksiyonu
+
+# while döngüsü
+# while içerisinde okul seçimi için koşul döngüsü
+
